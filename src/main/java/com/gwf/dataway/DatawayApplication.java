07773492826e54ac7625008b4a73cb711fef5859
RiverGod <2278417331@qq.com>
2023-01-07 11:36:15 +0800

    无改动

--- src/main/java/com/gwf/dataway/DatawayApplication.java
+++ src/main/java/com/gwf/dataway/DatawayApplication.java
@@ -5,6 +5,10 @@ import net.hasor.spring.boot.EnableHasorWeb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @authur RiverGods
+ * @date 2023/01/07 15:29
+ */
 @EnableHasor()
 @EnableHasorWeb()
 @SpringBootApplication(scanBasePackages = { "com.gwf.dataway.hasor" })
